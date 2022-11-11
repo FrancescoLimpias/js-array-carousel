53a8f08d4d2c5b0fd384e165ac65bd7dc2c5e444
Francesco Limpias <francescolimpias@gmail.com>
2022-11-11 17:23:33 +0100

    Bonus 2

--- bonus/main.js
+++ bonus/main.js
@@ -1,11 +1,13 @@
 // References
 const galleryElement = document.getElementById("gallery");
+const thumbsElement = document.getElementById("thumbs");
 const inputUp = document.getElementById("input-up");
 const inputDown = document.getElementById("input-down");
 
 // Gallery setup
 const nOfImages = 5;
 const gallery = [];
+const thumbs = [];
 
 // Active item
 let activeImage = 0;
@@ -25,6 +27,11 @@ for (let i = 0; i < nOfImages; i++) {
     // Add image to galleries
     galleryElement.append(image);
     gallery.push(image);
+
+    // Add image to thumbnails
+    let imageClone = image.cloneNode();
+    thumbsElement.prepend(imageClone);
+    thumbs.push(imageClone)
 }
 
 // Listen for clicks
@@ -32,6 +39,7 @@ inputUp.addEventListener("click", function () {
 
     // Hide old active image
     gallery[activeImage].classList.remove("active");
+    thumbs[activeImage].classList.remove("active");
 
     // Check for images on right
     if (activeImage == gallery.length - 1) {
@@ -43,12 +51,14 @@ inputUp.addEventListener("click", function () {
 
     // Dispay new active image
     gallery[activeImage].classList.add("active");
+    thumbs[activeImage].classList.add("active");
 
 });
 inputDown.addEventListener("click", function () {
 
     // Hide old active image
     gallery[activeImage].classList.remove("active");
+    thumbs[activeImage].classList.remove("active");
 
     // Check for images on right
     if (activeImage == 0) {
@@ -60,6 +70,7 @@ inputDown.addEventListener("click", function () {
 
     // Dispay new active image
     gallery[activeImage].classList.add("active");
+    thumbs[activeImage].classList.add("active");
 
 });
 
